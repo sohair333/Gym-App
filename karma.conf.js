b550ecb57357a83686410f10f6c632865c557289
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-07-17 19:03:40 +0200

    prepare for heroku

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gym-angular-app'),
+      dir: require('path').join(__dirname, './coverage/basic-angular-course'),
       subdir: '.',
       reporters: [
         { type: 'html' },
