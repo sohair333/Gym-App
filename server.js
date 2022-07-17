27eb9f39f511a5297373f3963aa0cfc3cd77bace
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-07-17 20:17:35 +0200

    .

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static(__direname + '/dist'));
 
 app.all('*',(req,res) =>{
-    res.status(200).sendFile(__dirname + '/dist/index.html');
+    res.status(200).sendFile(__dirname + '/dist/basic-angular-course');
 });
 app.listen(process.env.PORT || 8080);
